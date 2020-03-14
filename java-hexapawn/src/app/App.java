c04d95e81f7d7ed289d1961497133d601c23fac4
Dudrie <Dudrie@gmx.de>
2020-03-14 22:19:13 +0100

    Initial commit

---
+++ java-hexapawn/src/app/App.java
@@ -0,0 +1,8 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println(App.class.toString());
+        System.out.println("Hello Java");
+    }
+}
